894dc73d7b0b724007aa3e3afabdc2aedcf634de
Matt Wang <mw4854@nyu.edu>
2021-12-08 10:59:31 -0500

    Text gen

--- Assets/Matt/Scripts/TTextObjectGenerator.cs
+++ Assets/Matt/Scripts/TTextObjectGenerator.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Text.RegularExpressions;
 
 [ExecuteInEditMode]
 public class TTextObjectGenerator : MonoBehaviour
@@ -22,16 +24,30 @@ public class TTextObjectGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (activate)
-        // {
-        //     string[] texts = textBlock.Split(delimiters);
-        //     foreach (var t in texts)
-        //     {
-        //         string text = t.Trim();
-        //         Instantiate(textPrefab);
-        //         textPrefab.GetComponent<TTextInteraction>();
-        //     }
-        //     activate = false;
-        // }
+        if (activate)
+        {
+            string stippedBlock = RemoveBetween(textBlock, "[", "]");
+            string[] texts = textBlock.Split(delimiters, 1000000, System.StringSplitOptions.RemoveEmptyEntries);
+            foreach (var t in texts)
+            {
+                string text = t.Trim();
+                Instantiate(textPrefab);
+                textPrefab.GetComponent<TMP_Text>().text = text;
+            }
+            activate = false;
+        }
     }
+
+    public static string RemoveBetween(string original, string firstTag, string secondTag)
+{
+   string pattern = firstTag + "(.*?)" + secondTag;
+   Regex regex = new Regex(pattern, RegexOptions.RightToLeft);
+
+   foreach(Match match in regex.Matches(original))
+   {
+      original = original.Replace(match.Groups[1].Value, string.Empty);
+   }
+
+   return original;
+}
 }
