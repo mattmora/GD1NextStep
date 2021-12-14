b51522f6f0bd55e86088d0cf5890088aabf85507
Matt Wang <mw4854@nyu.edu>
2021-12-14 18:01:34 -0500

    putting together scenes

--- Assets/Hao/scripts/scene1Hole.cs
+++ Assets/Hao/scripts/scene1Hole.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class scene1Hole : MonoBehaviour
 {
@@ -14,13 +17,18 @@ public class scene1Hole : MonoBehaviour
     public float timeBeforeFall;
 
     public GameObject originalPlayer;
-    public GameObject cameraMoving;
-    public GameObject newPlayer;
+    // public GameObject cameraMoving;
+    // public GameObject newPlayer;
+
+    public Ease ease;
+    public Ease fallEase;
+
+    // public List<UnityEvent> onComplete;
+    public Scene scene2;
 
     void Start()
     {
-        Invoke("spawnNewWall", timer );
-        Invoke("movetheCamera", timeBeforeFall );
+
     }
 
     // Update is called once per frame
@@ -29,13 +37,30 @@ public class scene1Hole : MonoBehaviour
         
     }
 
+    public void RunAnimation()
+    {
+        transform.DORotate(new Vector3(0f, 360f, 0f), 3.33f, RotateMode.WorldAxisAdd).SetLoops(-1).SetEase(ease);
+        Camera.main.DOColor(Color.white, 1f).SetDelay(3.6f);
+        transform.DOMove(new Vector3(0f, 60f, 0f), 4.6f).SetEase(fallEase).SetDelay(0f).OnComplete(() =>
+        {
+            SceneManager.LoadScene("Evie_awakening");
+            // foreach (var cb in onComplete)
+            // {
+            //     cb.Invoke();
+                
+            // }
+        });
+        Invoke("spawnNewWall", timer );
+        Invoke("movetheCamera", timeBeforeFall);
+    }
+
     void spawnNewWall()
     {
         if (index < textWall.Length)
         {
-            textWall[index].alpha = 1;
+            textWall[index].DOFade(1f, 1f);
             //textWall[index].CrossFadeAlpha(1, 0.1f, true);
-            textWall[index].gameObject.SetActive(true);
+            // textWall[index].gameObject.SetActive(true);
             index += 1;
             timer -= timeGap;
             Invoke("spawnNewWall", timer);
@@ -45,17 +70,9 @@ public class scene1Hole : MonoBehaviour
 
     void movetheCamera()
     {
-        originalPlayer.SetActive(false);
-        cameraMoving.SetActive(true);
-        Invoke("fall", 2);
+        // originalPlayer.DOMove(landing.position, 2f);
+        // originalPlayer.SetActive(false);
+        // cameraMoving.SetActive(true);
+        // Invoke("fall", 2);
     }
-
-
-    void fall()
-    {
-        cameraMoving.SetActive(false);
-        newPlayer.SetActive(true);
-
-    }
-
 }
